e318aabe1fe80c23ce0e7713b47bfc46072ac51e
Georgi Serafimov <g.serafimov@c-trust.eu>
2024-04-18 21:29:01 +0300

    preserve or merge query params from prev url

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -23,6 +23,10 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit() {
-    this.router.navigate(['edit'], { relativeTo: this.route });
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+      preserveFragment: true,
+    });
   }
 }
